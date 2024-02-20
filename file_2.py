d58f00689baa8f07ef135c2866bb7900fdaa0500
Rauneet <vrauneet@gmail.com>
2024-02-20 16:36:09 +0530

    Added env value in .env

--- file_2.py
+++ file_2.py
@@ -3,11 +3,14 @@ import datetime
 from pytz import timezone
 import pprint
 import calendar
-import time 
+import time
+from dotenv import load_dotenv
+import os
 
+load_dotenv()        #this will load the environment variables from .env file
 # Constants 
-CLICKUP_API_TOKEN = 'pk_73223342_17LY9UC6TE84D6P5MF2ALXU5W8UT6LHA'  #clickup api token
-SLACK_WEBHOOK_URL = 'https://hooks.slack.com/triggers/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'  #slack webhook url-'https://hooks.slack.com/services/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'- previous webhook
+CLICKUP_API_TOKEN = os.getenv('CLICKUP_API_TOKEN')                                                                                            #'pk_73223342_17LY9UC6TE84D6P5MF2ALXU5W8UT6LHA'  #clickup api token
+SLACK_WEBHOOK_URL = os.getenv('SLACK_WEBHOOK_URL')                                                                                            #'https://hooks.slack.com/triggers/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'  #slack webhook url-'https://hooks.slack.com/services/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'- previous webhook
 CLICKUP_API_ENDPOINT = 'https://api.clickup.com/api/v2'       #clickup api endpoint
 HEADERS = {
     'Authorization': CLICKUP_API_TOKEN
@@ -21,7 +24,6 @@ def is_night_time():
     tz = timezone('Asia/Kolkata')
     current_time = datetime.datetime.now(tz)
     return not 9 <= current_time.hour <= 21
-
 
 
 def send_message_slack(message, task_url):
@@ -44,6 +46,30 @@ def send_message_slack(message, task_url):
 #     response = requests.post(SLACK_WEBHOOK_URL, json=payload)
 #     return response.status_code == 200
 
+
+# def is_request_type_bug(custom_fields):
+#     for field in custom_fields:
+#         if field.get('name') == 'Request Type ':
+#             bug_option_id = None
+#             for option in field.get('type_config', {}).get('option', []):
+#                 if option.get('name').lower() == 'bug':
+#                     bug_option_id = option.get('id')
+#                     break
+#             if bug_option_id and str(field.get('value')) == str(bug_option_id):
+#                 return True
+#     return False
+# def is_recent_tickets(ticket, days=30):
+#     ticket_updated = datetime.datetime.fromtimestamp(int(ticket['date_updated'])/1000)
+#     return datetime.datetime.now() - ticket_updated < datetime.timedelta(days=days)
+
+def is_bug_task(ticket):
+    request_type_field_id = 'af553c42-561b-4260-93b0-ca2afa6b520f'
+    bug_option_id = "2c234b21-fb9a-49ad-bceb-0a342556e213"                      #Comment out 
+    for custom_field in ticket.get("custom_fields", []):
+        if custom_field.get("id") == request_type_field_id and custom_field.get("value") == bug_option_id:
+            return True
+    return False
+
 def get_tasks_and_notify(list_id, list_name):
     if is_night_time():
         return            # Skip execution during night time
@@ -53,19 +79,24 @@ def get_tasks_and_notify(list_id, list_name):
         return             # Exit if tasks cannot be fetched
 
     tickets = response.json().get('tasks', [])
-    for ticket in tickets:
-        task_url = ticket.get('url')
-        # print(task_url)                              #for debugging
-
+    for ticket in tickets:    
+        task_url = ticket.get('url')     #this line will remain
+        
+        # print(task_url)                            #for debugging
         # is_bug = False
-        # for field in ticket.get('custom_field', []):                                                     
-        #     if field.get('name').strip() == 'Request Type' and field.get('value','').strip() == 'Bug':
-        #         is_bug = True
-        #         print(f"Found a Bug for Ticket ID: {ticket['id']}")
+        # for field in ticket.get('custom_fields', []):
+        #     if field.get('name') == 'Request Type ':
+        #         if 'value' in field and field.get('value') is not None:
+        #             for option in field.get('type_config', {}).get('options', []):
+        #                 if option.get('name').lower() == 'bug' and str(field.get('value')) == str(option.get('id')):
+        #                     is_bug = True
+        #                     break
         #         break
-        # if not is_bug:
-        #     print(f"Skipping Ticket ID: {ticket['id']} as it's not marked as a Bug.")
-        #     continue
+        # if is_bug:
+        #     task_url = ticket.get('url')
+        #     print(f"Bug Ticket URL: {task_url}")
+            
+                   
 
         status_type = ticket.get('status', {}).get('status', '').lower().replace(" ", "")
         #print(f"Debug - Ticket ID: {ticket['id']} Status: {status_type}") 
@@ -84,7 +115,7 @@ def get_tasks_and_notify(list_id, list_name):
                 comments = comment_response.json().get('comments' ,[])
                 pprint.pprint(comments)
                 # Checks if there are more than two comments on the task.
-                if len(comments) >2:
+                if len(comments) > 2:
                     print(f"Task ID: {task_id} has more than two comments, no action needed.")
                     continue
                 #checks if there are comments and check the time of the last comment 
@@ -97,8 +128,8 @@ def get_tasks_and_notify(list_id, list_name):
                     if(current_time - last_comment_timestamp) > 7200:
                         message = f'Ticket ID: {task_id} from list "{list_name}" with status "{status_type}" and priority "{priority_type}" requires attention.'  # this is included https://app.clickup.com/t/{task_id}
                         # task_url =  f'https://app.clickup.com/t/{task_id}'    
-                        if send_message_slack(message,task_url):
-                            print(f'Message being sent' , message)
+                        if send_message_slack(message,task_url):                                              #inten
+                            print(f'Message being sent' , message)                                            #inten
                         else:
                             print(f'Failed to send notification!!')
                     else:
