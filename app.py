540fdc7a35a3aa399b071bcb1cd96c7ebb87f921
Rauneet <vrauneet@gmail.com>
2024-02-27 14:10:26 +0530

    Added function to send summary

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from file_2 import get_tickets_from_customer_lists, is_night_time
 app = Flask(__name__)
 @app.route('/test-tasks')
 def test_tasks():
-     if is_night_time():
-        return "It's night time. No operations will be performed."
+    if not is_night_time():
+        get_tickets_from_customer_lists('109448264')
+        return "Tickets checked and send notification"
+    else:
+        return "Its night time no action needed"
+
 thread = threading.Thread(target=get_tickets_from_customer_lists, args=("109448264",))
 thread.start()
 if __name__ == '__main__':
