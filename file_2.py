5beb057d315dbb07f18c374e7e50e53605f4f120
Rauneet <vrauneet@gmail.com>
2024-02-27 17:33:33 +0530

    added send summary slack

--- file_2.py
+++ file_2.py
@@ -11,6 +11,9 @@ import os
 load_dotenv()       #this will load the environment variables from .env file
 #dictionary to keep track of notified tickets 
 notified_tickets = set()
+# Global list to keep track of tickets for end-of-day summary
+end_of_day_tickets = []   #new line
+# summary_sent_today = False
 
 
 # Constants 
@@ -29,9 +32,8 @@ def is_night_time():
     current_time = datetime.datetime.now(tz)
     return not 9 <= current_time.hour <= 21
 
-
 def send_message_slack(message, task_url):
-    full_message = f'{message} Ticket URL:'
+    full_message = f'{message}'                 #Ticket URL:is included in full message
     payload = {
         'full_message' : full_message,
         'task_url': task_url
@@ -52,21 +54,15 @@ def is_bug_based_on_comment(comments):
         return True
     else:
         return False     #it is not a bug based on comment
+#checks if the tcket is created or updated within last "hours"
+# def is_recent_ticket(ticket, hours=24):
+#     now = datetime.datetime.now(datetime.timezone.utc)   #gets the current time and date
+#     date_updated = datetime.datetime.fromtimestamp(int(ticket['date_updated']) /1000 , tz=datetime.timezone.utc)
+#     date_created = datetime.datetime.fromtimestamp(int(ticket['date_created']) /1000 , tz=datetime.timezone.utc)
+#     updated_recently = (now - date_updated) < datetime.timedelta(hours=hours)
+#     created_recently = (now - date_created) < datetime.timedelta(hours=hours)
 
-# def is_request_type_bug(custom_fields):
-#     for field in custom_fields:
-#         if field.get('name') == 'Request Type ':
-#             bug_option_id = None
-#             for option in field.get('type_config', {}).get('option', []):
-#                 if option.get('name').lower() == 'bug':
-#                     bug_option_id = option.get('id')
-#                     break
-#             if bug_option_id and str(field.get('value')) == str(bug_option_id):
-#                 return True
-#     return False
-# def is_recent_tickets(ticket, days=30):
-#     ticket_updated = datetime.datetime.fromtimestamp(int(ticket['date_updated'])/1000)
-#     return datetime.datetime.now() - ticket_updated < datetime.timedelta(days=days)
+    # return updated_recently or created_recently
 
 def get_tasks_and_notify(list_id, list_name):
     global notified_tickets                                   #global notified_tickets dictionary for storing the ticket id which have been notified 
@@ -81,24 +77,10 @@ def get_tasks_and_notify(list_id, list_name):
     for ticket in tickets:
         task_id = ticket['id']                                                                         #till here 
         task_url = ticket.get('url')     #this line will remain
-        if task_id in notified_tickets:      #checks if the ticket is in notified_tickets dictionary if not present it will again continue 
+        if task_id in notified_tickets: #or not is_recent_ticket(ticket):      #checks if the ticket is in notified_tickets dictionary if not present it will again continue 
             continue
         # print(task_url)                            #for debugging
-        # is_bug = False
-        # for field in ticket.get('custom_fields', []):
-        #     if field.get('name') == 'Request Type ':
-        #         if 'value' in field and field.get('value') is not None:
-        #             for option in field.get('type_config', {}).get('options', []):
-        #                 if option.get('name').lower() == 'bug' and str(field.get('value')) == str(option.get('id')):
-        #                     is_bug = True
-        #                     break
-        #         break
-        # if is_bug:
-        #     task_url = ticket.get('url')
-        #     print(f"Bug Ticket URL: {task_url}")
-            
-                   
-
+        
         status_type = ticket.get('status', {}).get('status', '').lower().replace(" ", "")
         #print(f"Debug - Ticket ID: {ticket['id']} Status: {status_type}") 
         priority = ticket.get('priority')    #get to the priority object  and then access the attribute value
@@ -106,7 +88,7 @@ def get_tasks_and_notify(list_id, list_name):
         priority_type = priority.get('priority', '').lower() if priority and isinstance(priority, dict) else 'none'
         
         # Check for desired status and priority
-        if status_type in ['open','inprogress','pending(ack)'] and priority_type in ['high', 'urgent']:
+        if status_type in ['open','inprogress','pending(ack)','planned'] and priority_type in ['high', 'urgent']:
             # task_id = ticket['id']
             # task_url = f"https://app.clickup.com/t/{task_id}"
          # Fetches comments for a given task from ClickUp   
@@ -115,6 +97,7 @@ def get_tasks_and_notify(list_id, list_name):
                 # Extracts comments
                 comments = comment_response.json().get('comments' ,[])
                 if is_bug_based_on_comment(comments):
+                    #end_of_day_tickets.append(task_url)   #new line is added here      #identified as bug and added to the list 
                 #filter the comments made by bot
                     user_comments = [comment for comment in comments if comment['user']['id'] != -1]
                     pprint.pprint(comments)
@@ -132,8 +115,9 @@ def get_tasks_and_notify(list_id, list_name):
                         if(current_time - last_comment_timestamp) > 7200:
                             message = f'Update with latest progress.'  # this is included https://app.clickup.com/t/{task_id}
                         # task_url =  f'https://app.clickup.com/t/{task_id}'    
-                            if send_message_slack(message,task_url):                                              
+                            if send_message_slack(message, task_url):                                              
                                 print(f'Message being sent' , message)
+                                end_of_day_tickets.append(task_url)
                                 notified_tickets.add(task_id)                                  #[task_id] = True                        #this will mark the ticket as notified true                                        
                             else:
                                 print(f'Failed to send notification!!')
@@ -142,10 +126,10 @@ def get_tasks_and_notify(list_id, list_name):
                     else:
                         print(f'No comments in the ticket is made by user')   # No comments have been made on the task.
                 else:
-                    print(f'its not a bug based on comment {task_url}')
+                    print(f'its not a bug based on comment {task_url}') #commented out this but we have to un comment thhis 
             else:
                 print(f'failed to fetch ticket comments')    #Failed to fetch the task comments
-            
+                                       
                     
 # retrieves the lists from the specific folder in the clickup
 def get_list(folder_id):
@@ -162,11 +146,31 @@ def get_tickets_from_customer_lists(folder_id):
         print(f'Fetching tickets for list: {list_name}')
         get_tasks_and_notify(list_id,list_name)
 
+def send_summary_slack():
+    global end_of_day_tickets, summary_sent_today
+    if end_of_day_tickets:
+        message = "End of Day Summary: Bug Tickets\n"
+        message +="\n".join([f"- {task_url}" for task_url in end_of_day_tickets])
+        if send_message_slack(message, None):
+            print(f"Summary sent to slack")
+            # summary_sent_today = True
+            end_of_day_tickets.clear()
+        else:
+            print('Summary not sent to slack')
+    else:
+        print(f'No bug tickets for the day')
+                                    
 if not is_night_time():
     # it will check for new tickets every 60 min
     while True:                          #1
         print('checking for new tickets to notify')  #2
         get_tickets_from_customer_lists("109448264")
+        # current_time = datetime.datetime.now()
+        # if current_time.hour == 20 and current_time.minute == 59 and not summary_sent_today:           #time is 21 <=current_time.hour < 22
+        send_summary_slack()
+            # summary_sent_today = True
+        # elif current_time.hour < 20 or current_time.hour >= 22:
+        #     summary_sent_today = False
         print(f"Waiting for {SECONDS_IN_AN_HOUR // 60} minutes until the next check.")  #3  
         time.sleep(SECONDS_IN_AN_HOUR)  #4
 else:
